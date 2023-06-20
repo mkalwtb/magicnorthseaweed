9195c2d7ff0d0c6d8dd4ee1f7e7f1a9098be26c4
M. Al <al@sowento.com>
2023-06-20 18:28:28 +0200

    Add description for adding buoy data

--- data/rijkswaterstaat.py
+++ data/rijkswaterstaat.py
@@ -28,6 +28,16 @@ def _locate_waarde(old: pd.DataFrame, data_name: str, value_col: str) -> pd.Data
 
 @dataclass
 class BoeiData:
+    """
+    Om de boei data op te zoeken:ga naar https://waterinfo.rws.nl/#!/details/publiek/waterhoogte/
+    1.
+    2. Selecteer de boei & data die je wil
+    3. click Export/Delen
+    4. Right click op CSV
+    5. copy link adress
+    6. Extract de parameter=... en location_slug=... en time_horizon=...
+    7. Indien nodig, open de csv file en kijk hoe de colom heet. Normaal is dit Waarde
+    """
     name: str
     parameter: str
     locoation_slug: str
