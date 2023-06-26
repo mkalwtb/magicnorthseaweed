760dd0f739f779bd9be1408823566773acd7d9cc
M. Al <al@sowento.com>
2023-06-26 23:22:06 +0200

    Add info on how to set up new bouys

--- data/boeien.py
+++ data/boeien.py
@@ -3,11 +3,21 @@ from matplotlib import pyplot as plt
 from pathlib import Path
 from rijkswaterstaat import Boei, BoeiData
 
+"""
+Om de boei data op te zoeken:
+1. Ga naar https://waterinfo.rws.nl/#!/details/publiek/waterhoogte/
+2. Selecteer de boei & data die je wil
+3. click Export/Delen
+4. Right click op CSV
+5. copy link adress
+6. Extract de parameter=... en location_slug=... en time_horizon=...
+7. Indien nodig, open de csv file en kijk hoe de colom heet. Normaal is dit Waarde
+"""
+
 time_48h48h = "-48,48"
 time_48h0h = "-48,0"
 time_28d = "-672,0"
 timing = time_28d
-# Link: https://waterinfo.rws.nl/#!/kaart/wind/
 
 ijmuiden = Boei(parameters=
                 [
