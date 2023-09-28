3e3c1c7eeefb38b58e1806eb095d9725ab00774a
M. Al <al@sowento.com>
2023-09-28 19:42:27 +0200

    refactoring

--- data/stormglass.py
+++ data/stormglass.py
@@ -10,8 +10,7 @@ from datetime import datetime
 import pytz
 
 file = Path('data.pkl')
-channels = ['waveDirection', 'wavePeriod', "waveHeight", "windSpeed", 'windDirection',
-            "seaLevel", "windWaveHeight"]  # "currentSpeed"
+channels = ['waveDirection', 'wavePeriod', "waveHeight", "windSpeed", 'windDirection', "windWaveHeight", "currentSpeed"]  # "currentSpeed"
 
 # Get first hour of today
 def download_json(lat, long, start, end, cache=False):
@@ -33,9 +32,9 @@ def download_json(lat, long, start, end, cache=False):
         'end': end.to('UTC').timestamp()  # Convert to UTC timestamp
       },
       headers={
-        # 'Authorization': '1feeb6a8-5bc9-11ee-a26f-0242ac130002-1feeb702-5bc9-11ee-a26f-0242ac130002'
+        'Authorization': '1feeb6a8-5bc9-11ee-a26f-0242ac130002-1feeb702-5bc9-11ee-a26f-0242ac130002'
         # 'Authorization': '5bf98f1a-2979-11ee-8d52-0242ac130002-5bf98f88-2979-11ee-8d52-0242ac130002'
-        'Authorization': 'a5396776-5d64-11ee-8b7f-0242ac130002-a53967da-5d64-11ee-8b7f-0242ac130002'
+        # 'Authorization': 'a5396776-5d64-11ee-8b7f-0242ac130002-a53967da-5d64-11ee-8b7f-0242ac130002'
       }
     )
 
@@ -66,11 +65,7 @@ def json_to_df(json_data):
 def download_and_save_data(lat, long, start, end, cache=False):
     json_data = download_json(lat=lat, long=long, start=start, end=end, cache=cache)
     data_new = json_to_df(json_data)
-    if file.is_file():
-        data_db = pd.read_pickle(file)
-        data_new = pd.concat([data_db, data_new], axis=0)
-    if len(data_new) > 0:  # save data
-        data_new.to_pickle(file)
+    append_historical_data(lat, long, data_new)
     return data_new
 
 def load_data(lat, long):
@@ -80,29 +75,34 @@ def load_data(lat, long):
         data_db = pd.DataFrame()
     return data_db
 
-
-def load_and_download_data(lat, long, start, end, cache=False):
-    # Timing
+def append_historical_data(lat, long, data_new):
+    """Only function with writing acces"""
     now = datetime.now()
     data_db = load_data(lat, long)
+    tz = data_new.index.tz
+    now_pd = pd.to_datetime(now, utc=tz)
+    data_new_historical = data_new[data_new.index <= now_pd]
+    data = pd.concat([data_db, data_new_historical], axis=0)
+    if len(data_new) > 0:  # save data
+        data.to_pickle(file)
+    return data_new
 
 
+def smart_data(lat, long, start, end, cache=False):
+    data_db = load_data(lat, long)
     start_in_df = arrow.get(data_db.index.min()) <= start
     end_in_df = arrow.get(data_db.index.max()) >= end
-    if not start_in_df or not end_in_df:
-        print("Scraping new data")
+    request_is_fully_in_df = start_in_df and end_in_df
+
+    if request_is_fully_in_df:
+        print("Data solely from database")
+        result = data_db
+    else:
         json_data = download_json(lat=lat, long=long, start=start, end=end, cache=cache)
         data_new = json_to_df(json_data)
-        tz = data_new.index.tz
-        now_pd = pd.to_datetime(now, utc=tz)
-        data_new_historical = data_new[data_new.index <= now_pd]
-        if len(data_new_historical) > 0:  # save data
-            data_db = pd.concat([data_db, data_new_historical], axis=0)
-            data_db.to_pickle(file)
-        result = pd.concat([data_db, data_new], axis=0)
-    else:
-        result = data_db
+        result = append_historical_data(lat, long, data_new)
 
+    #Requested time range of the data
     tz = result.index.tz
     start_pd = pd.to_datetime(start.datetime, utc=tz)
     end_pd = pd.to_datetime(end.datetime, utc=tz)
@@ -110,31 +110,40 @@ def load_and_download_data(lat, long, start, end, cache=False):
     return result_range
 
 
-def append_x_days_upfront(lat, long, days, cache=False):
+def append_x_days_upfront(lat, long, days: float, cache=False):
     data = load_data(lat, long)
-    oldest = arrow.get(min(data.index))
+    if len(data) > 0:
+        oldest = arrow.get(min(data.index))
+    else:
+        oldest = arrow.now('Europe/Amsterdam')
     new_oldest = oldest.shift(days=-days)
     return download_and_save_data(lat, long, new_oldest, oldest, cache=cache)
+
+
+def forecast(lat, long, hours, cache=False):
+    now = arrow.now('Europe/Amsterdam')
+    start = now.shift(hours=0)
+    end = now.shift(hours=hours)
+    json_data = download_json(lat=lat, long=long, start=start, end=end, cache=cache)
+    data_new = json_to_df(json_data)
+    return data_new
 
 
 if __name__ == '__main__':
     lat = 52.464295
     long = 4.532720
     now = arrow.now('Europe/Amsterdam')
-    start = now.shift(hours=0)
-    end = now.shift(hours=48)
+    start = now.shift(hours=-45)
+    end = now.shift(hours=0)
     cache=False
 
     # Forecast
-    # json_data = download_json(lat, long, start, end, cache=True)
-    # df = json_to_df(json_data)
 
     # Historical
-    # download_and_save_data(lat, long, start, end, cache=cache)
 
-    df = append_x_days_upfront(lat, long, 10, cache=cache)
-    # df = load_data(lat, long)
-    # df = load_from_file_or_download(lat, long, start, end, cache=cache)
+    # df = append_x_days_upfront(lat, long, 10, cache=cache)
+    df = load_data(lat, long)
+    # df = forecast(lat, long, 24, cache=cache)
 
     # Plot
     # print(tabulate(df, headers='keys', tablefmt='psql'))
