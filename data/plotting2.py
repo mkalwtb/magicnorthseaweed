085edbfa8f0d51521098578d6bb1c9ed25a51b65
M. Al <al@sowento.com>
2023-12-18 19:58:50 +0100

    Split up Model and Spot

---
+++ data/plotting2.py
@@ -0,0 +1,171 @@
+from math import radians, cos, sin
+
+import pandas as pd
+from tabulate import tabulate
+from plotting import angle_to_direction
+from plotting import website_folder
+
+hex_colors = [
+    "rgba(255, 0, 0, 0.0)", "rgba(255, 0, 0, 0.0)", "rgba(255, 0, 0, 0.0)",
+    "#e31a1c",  # Red for scale
+    "#fc7d23",  # Orange for scale
+    "#fdd036",  # Yellow for scale
+    # "#d2d550", #
+    "#a6d96a",  # Light green for scale 7
+    "#86cb67", # 8 light green
+    # "#76c465",  # 8 medium
+    "#66bd63",  # 9 green
+    "#800080"  # purple for scale 10
+]
+
+def replace_last_comma_by_and(string):
+    delimiter = ", "
+    replacement = " en "
+    if delimiter in string:
+        start, _, end = string.rpartition(delimiter)
+        return start + replacement + end
+    else:
+        return string
+
+
+def perk_identification(row):
+    perks = []
+    # if row["krachtig"] > 2:
+    #     perks.append("krachtig")
+    if row["clean"] <= 1:
+        if row["hoog"] >= 2.5 and row["krachtig"] >= 2:
+            perks.append("stormachtig")
+        elif row["hoog"] >= 1.5:
+            perks.append("klotsbak")
+        else:
+            perks.append("rommel")
+    elif row["clean"] >= 2:
+        perks.append("clean")
+
+    if row["hoog"] <= 0.3:
+        perks.append("flat")
+    elif row["hoog"] <= 1.2:
+        perks.append("klein")
+    elif row["hoog"] >= 2:
+        perks.append("hoog")
+
+    if row["stroming"] > 2:
+        perks.append("stroming")
+    return perks
+
+def html_arrow(angle):
+    angle_name = angle_to_direction(angle)
+    return f"<span class='grey' title='{angle:.0f} deg''>{angle_name}</span>"
+    # return f"<b title='{angle_name} ({angle:.0f} deg)' style='height: 18px; transform: rotate({angle+180:.0f}deg);'>&uarr;</b>"
+    # return f"<i  class='fa fa-arrow-left' style='transform: rotate({angle+180:.0f}deg);'></i>"
+
+def table_html(df):
+    columns_df = ["rating", "waveHeight", "wavePeriod", "windSpeed"]
+    # columns = ["rating", "waveHeight", "waveDirection", "wavePeriod", "windSpeed", "windDirection"]
+    headers = ["Tijd", "rating", "golven", "wind", "getij", "beschrijving"]
+
+    html = """
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <style>
+        table {
+            min-width: 600px;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+    
+        th, td {
+            text-align: center;
+            white-space: nowrap;
+            vertical-align: middle;
+            /* border: 1px solid #ddd; Adding a border for better visibility */
+        }
+        
+        h3 {
+          display: inline-block; /* Make the h3 inline-block */
+          margin: 5px; /* Add some margin between the div and h3 */
+        }
+    
+        th {
+            background-color: #f2f2f2;
+        }
+        .grey {
+            color: rgb(152, 162, 175);
+        }
+        .unit {
+            font-size: 14px;
+            padding-right: 12px;
+        }
+        .rounded-span {
+            height: 20px; /* Adjust the height as needed */
+            width: 5px; /* Adjust the height as needed */
+            border-radius: 2.5px; /* Adjust the border-radius for round corners */
+            display: inline-block;
+        }
+        
+    </style>
+    """
+
+    # header
+    html += f"<h2> {df.index[0].strftime('%A, %d-%m')} </h2>"
+    html += "<table>\n"
+    html += "<tr>\n"
+    for header in headers:
+        html += f"<th>{header}</th>\n"
+    html += "</tr>\n"
+
+    # rows
+    df = df.between_time('08:00', '17:00')
+    for index, row in df.iterrows():
+        color = hex_colors[round(row['rating'])-1]
+        color_bar = f"<div class='rounded-span'  style='background-color: {color}'></div>"
+
+        html += "<tr>\n"
+        html += f"\t<td>{index.strftime('%H:%M')}</td>\n"
+        html += f"\t<td> {color_bar} <h3>{row['rating']:.0f}</h3></td>\n"
+
+        div = "<td>"
+        div += f"<b>{row['waveHeight']:.1f} </b> <i class='unit'>m</i>"
+        div += f"<b>{row['wavePeriod']:.0f} </b> <i class='unit'>s</i>"
+        div += html_arrow(row["waveDirection"])
+        div += "</td>"
+        html += div
+
+        html += f"\t<td>{row['windSpeed']*3.6:.0f}  <i class='unit'>km/h</i>"
+        html += f"{html_arrow(row['windDirection'])}</td>"
+
+        html += f"\t<td>{10*round(10*row['NAP']):.0f} <i class='unit'>cm</i></td>"
+
+        html += "<td>"
+        perks = perk_identification(row)
+        html += replace_last_comma_by_and(", ".join(perks))
+        html += "</td>"
+
+        html += "</tr>\n"
+    html += "</table>\n"
+    return html
+
+def table_per_day(df):
+    df_days = [group[1] for group in df.groupby(df.index.date)]
+    # table(df_days[1])
+
+    html = ""
+    for df_day in df_days:
+        html += table_html(df_day)
+    return html
+
+
+def write_table_per_day(df, spot_name):
+    html = table_per_day(df)
+    with open(website_folder / "tables" / f"table_{spot_name}.html", "w") as fp:
+        fp.write(html)
+
+
+if __name__ == "__main__":
+    df = pd.read_pickle("df.pkl")
+
+    html = table_per_day(df)
+    with open("table_ZV.html", "w") as fp:
+        fp.write(html)
+
+    # table_per_day(df, "ZV")
+
--- data/spots.py
+++ data/spots.py
@@ -3,21 +3,14 @@ import numpy as np
 from copy import deepcopy, copy
 import datetime
 import re
-import pickle
 from dataclasses import dataclass
 from matplotlib import pyplot as plt
-from pathlib import Path
 
-import xgboost as xgb
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from tabulate import tabulate
+from data.models import Model, MODELS
 from plotting import plot_forecast
 
-import boeien, surffeedback, stormglass
-from rijkswaterstaat import Boei
+import surffeedback, stormglass
 
-forecast_columns = ["rating", "hoog", "clean", "krachtig", "stijl", "stroming", "windy"]
 
 def _compute_onshore(data: pd.DataFrame, richting: float, side_shore) -> pd.DataFrame:
     if side_shore:
@@ -40,56 +33,7 @@ def _enrich_input_data(data: pd.DataFrame, richting: float) -> pd.DataFrame:
 
     return data
 
-class Model:
-    def __init__(self, perk: str, model=None):
-        self.perk = perk
-        self.model: xgb.XGBModel = model if model is not None else self._load_model()
 
-    @classmethod
-    def train(cls, spot, channel, verbose=False, save=True, only_spot_data=True, match_all_feedback_times=True):
-        """Train a model, save it and returns the model"""
-        df = spot.combined(only_spot_data=only_spot_data, match_all_feedback_times=match_all_feedback_times,
-                           fb_columns=channel)
-        # TODO add loop, this line for each spot
-
-        X = df.drop(channel, axis=1)
-        y = df[channel]
-
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-        model = xgb.XGBRegressor(objective='reg:squarederror')
-        model.fit(X_train, y_train)
-
-        y_pred = model.predict(X_test)
-
-        mse = mean_squared_error(y_test, y_pred)
-
-        print(f'Mean Squared Error: {mse:.2f} (from {len(df)} feedback entries)')
-        if verbose:
-            for i in range(len(y_test)):
-                print('real:', y_test[i], 'pred:', y_pred[i])
-        obj = cls(perk=channel, model=model)
-        if save:
-            obj.save_model()
-        return obj
-
-    def _load_model(self):
-        if not self.model_file.is_file():
-            raise NotImplementedError("Use .train() first to train a model")
-        with open(self.model_file, 'rb') as f:
-            model = pickle.load(f)
-        return model
-
-    def save_model(self):
-        with open(self.model_file, 'wb') as f:
-            pickle.dump(self.model, f)
-
-    @property
-    def model_file(self):
-        return Path(f"AI-models/model_XGBRegressor_ZV_{self.perk}.pkl")  # todo remove ZV
-
-
-MODELS = [Model(perk) for perk in forecast_columns]
 
 
 @dataclass
